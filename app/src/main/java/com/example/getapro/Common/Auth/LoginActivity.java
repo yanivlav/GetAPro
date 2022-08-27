e9af85d5b1be767b845a75ecfd6b898ac01aa804
yanivlav <yan465@gamil.com>
2022-08-27 22:09:35 +0300

    added firebase connection to LoginActivity

--- app/src/main/java/com/example/getapro/Common/Auth/LoginActivity.java
+++ app/src/main/java/com/example/getapro/Common/Auth/LoginActivity.java
@@ -2,27 +2,56 @@ package com.example.getapro.Common.Auth;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.getapro.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     CoordinatorLayout coordinatorLayout;
+
+    String fullname;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    FirebaseAuth.AuthStateListener authStateListener;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(authStateListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +61,49 @@ public class LoginActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navigation_view);
         coordinatorLayout = findViewById(R.id.coordinator);
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                View headerView = navigationView.getHeaderView(0);
+                TextView userTv = headerView.findViewById(R.id.navigation_header_text_view);
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){//logon
+                    if(fullname!= null) {//signup = update profile with full name
+                        user.updateProfile(new UserProfileChangeRequest.Builder().setDisplayName(fullname).build()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                fullname = null;
+                                if (task.isSuccessful())
+                                    Snackbar.make(coordinatorLayout,user.getDisplayName() + "welcome", Snackbar.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    userTv.setText(user.getDisplayName() + "logged in");
+
+                    navigationView.getMenu().findItem(R.id.item_login).setVisible(false);
+                    navigationView.getMenu().findItem(R.id.item_signup).setVisible(false);
+                    navigationView.getMenu().findItem(R.id.item_logout).setVisible(true);
+
+                    //read the user database
+
+                }
+                else{
+                    userTv.setText("please Login");
+                    //collapsing?
+                    navigationView.getMenu().findItem(R.id.item_login).setVisible(true);
+                    navigationView.getMenu().findItem(R.id.item_signup).setVisible(true);
+                    navigationView.getMenu().findItem(R.id.item_logout).setVisible(false);
+
+                    //adapter.notifyDataSetChanged();
+
+                }
+
+
+            }
+        };
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -45,7 +117,67 @@ public class LoginActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 item.setChecked(false);
                 drawerLayout.closeDrawers();
-                Toast.makeText(LoginActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(LoginActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                View dialogView = getLayoutInflater().inflate(R.layout.sign_dialog,null);
+
+                EditText usernameEt = dialogView.findViewById(R.id.username_input);
+                EditText fullnameEt = dialogView.findViewById(R.id.fullname_input);
+                EditText passwordEt = dialogView.findViewById(R.id.password_input);
+
+                switch (item.getItemId()){
+                    case R.id.item_signup:
+                        builder.setView(dialogView).setPositiveButton("Register", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                String username = usernameEt.getText().toString();
+                                fullname = fullnameEt.getText().toString();
+                                String password = passwordEt.getText().toString();
+
+                                //signup to user
+                                firebaseAuth.createUserWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if(task.isSuccessful())
+                                            Snackbar.make(coordinatorLayout,"Signup success",Snackbar.LENGTH_SHORT);
+                                        else
+                                            Snackbar.make(coordinatorLayout,"Signup failed",Snackbar.LENGTH_SHORT);
+                                    }
+                                });
+                            }
+                        }).show();
+                        break;
+
+                    case R.id.item_login:
+                        fullnameEt.setVisibility(View.GONE);
+                        builder.setView(dialogView).setPositiveButton("Login", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                String username = usernameEt.getText().toString();
+                                String password = passwordEt.getText().toString();
+
+                                //login to user
+                                firebaseAuth.signInWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if(task.isSuccessful())
+                                            Snackbar.make(coordinatorLayout,"Signup success",Snackbar.LENGTH_SHORT);
+                                        else
+                                            Snackbar.make(coordinatorLayout,"Signup failed",Snackbar.LENGTH_SHORT);
+                                    }
+                                });
+                            }
+
+                        }).show();
+                        break;
+
+                    case R.id.item_logout:
+                        firebaseAuth.signOut();
+                        break;
+                }
+
+
                 return true;
             }
         });
@@ -67,6 +199,8 @@ public class LoginActivity extends AppCompatActivity {
 
 
     }
+
+//    CollapsingToolbarLayout collapsing = findViewById(R.id.)
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {

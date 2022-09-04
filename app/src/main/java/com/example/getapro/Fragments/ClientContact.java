61d7c23ca62171a05e4a0b2bcd0d00c13d472c6e
yanivlav <yan465@gamil.com>
2022-09-04 17:14:47 +0300

    deleting extra files

--- app/src/main/java/com/example/getapro/Fragments/ClientContact.java
+++ app/src/main/java/com/example/getapro/Fragments/ClientContact.java
@@ -148,7 +148,7 @@ public class ClientContact extends Fragment {
         locationB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Navigation.findNavController(view).navigate(R.id.action_clientContact_to_blankFragment);
+
             }
         });
 
--- app/src/main/java/com/example/getapro/MainActivity.java
+++ app/src/main/java/com/example/getapro/MainActivity.java
@@ -10,15 +10,12 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.example.getapro.Common.Location;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;

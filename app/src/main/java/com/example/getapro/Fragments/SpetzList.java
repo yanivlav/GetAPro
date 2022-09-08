f525a87f42b35a2657ea8058a79f1ec37fb8bdce
BarDAP <barbenshabat@gmail.comgit config --global user.name BarDAP>
2022-09-08 18:21:30 +0300

    Spets requests massage fragment with brodcast reciver

--- app/src/main/java/com/example/getapro/Fragments/SpetzList.java
+++ app/src/main/java/com/example/getapro/Fragments/SpetzList.java
@@ -1,26 +1,55 @@
 package com.example.getapro.Fragments;
 
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Parcelable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.getapro.Helpers.SpetzAdapter;
+import com.example.getapro.MainActivity;
 import com.example.getapro.MyObjects.Spetz;
 import com.example.getapro.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SpetzList extends Fragment {
 
     ArrayList<Spetz> spetzs;
+    FirebaseMessaging messaging = FirebaseMessaging.getInstance();
+//    BroadcastReceiver receiver;
+    String TAG = "MyFirebaseInstanceIdService";
+    String token;
 
 
     public static SpetzList newInstance(String param1, String param2) {
@@ -39,6 +68,29 @@ public class SpetzList extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.spetz_list, container, false);
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @SuppressLint("LongLogTag")
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            System.out.println("Fetching FCM registration token failed");
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        token = task.getResult();
+
+                        // Log and toast
+                        System.out.println(token);
+                        Log.d(TAG, "Refreshed token: " + token);
+                        Toast.makeText(getContext(), "Device Token is: " + token, Toast.LENGTH_SHORT).show();
+//                        mtv.setText(token);
+                    }
+                });
+//        messaging.unsubscribeFromTopic("A");
+//        messaging.unsubscribeFromTopic("B");
 
         if (spetzs==null) {
             spetzs = new ArrayList<>();
@@ -65,17 +117,75 @@ public class SpetzList extends Fragment {
             @Override
             public void onSpetzClicked(int position, View view) {
 //                Open Chat
+                messaging.unsubscribeFromTopic(spetzs.get(position).getUserName());
+                messaging.subscribeToTopic(spetzs.get(position).getUserName());
+
+
+                //Form
+                String textToSend = "Add the user form request!";
+
+                final JSONObject rootObject = new JSONObject();
+
+                try {
+                    rootObject.put("to", spetzs.get(position).getUserName());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    rootObject.put("data", new JSONObject().put("message", textToSend));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+                String url = "https://fcm.googleapis.com/fcm/send";
+
+                RequestQueue queue = Volley.newRequestQueue(getContext());
+                StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }) {
+
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> headers = new HashMap<>();
+                        headers.put("Content-Type", "application/json");
+                        headers.put("Authorization", "key=" + token);
+//                            headers.put("Authorization","key="+API_TOKEN_KEY);
+                        return headers;
+                    }
+
+                    @Override
+                    public byte[] getBody() throws AuthFailureError {
+                        return rootObject.toString().getBytes();
+                    }
+                };
+                queue.add(request);
+                queue.start();
             }
+
 
             @Override
             public void onSpetzLongClicked(int position, View view) {
 
             }
         });
-            recyclerView.setAdapter(formAdapter);
+
+        recyclerView.setAdapter(formAdapter);
+
 
             return view;
     }
+
+
 }
 
 
--- app/src/main/java/com/example/getapro/Services/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/getapro/Services/MyFirebaseMessagingService.java
@@ -1,39 +1,55 @@
 package com.example.getapro.Services;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-public class MyFirebaseMessagingService extends FirebaseMessagingService{
-//    @Override
-//    public void onMessageReceived(RemoteMessage remoteMessage) {
-//        // ...
-//
-//        // TODO(developer): Handle FCM messages here.
-//        Log.d(TAG, "From: " + remoteMessage.getFrom());
-//
-//        // Check if message contains a data payload.
-//        if (remoteMessage.getData().size() > 0) {
-//            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-//
-//            if (/* Check if data needs to be processed by long running job */ true) {
-//                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-//                scheduleJob();
-//            } else {
-//                // Handle message within 10 seconds
-//                handleNow();
-//            }
-//
-//        }
-//
-//        // Check if message contains a notification payload.
-//        if (remoteMessage.getNotification() != null) {
-//            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-//        }
-//
-//        // Also if you intend on generating your own notifications as a result of a received FCM
-//        // message, here is where that should be initiated. See sendNotification method below.
-//    }
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
+    final String TAG = "MyFirebaseMessaging";
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            Intent intent = new Intent("message_received");
+            intent.putExtra("message",remoteMessage.getData().get("message"));
+            LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+
+            //if the application is not in forground post notification
+            NotificationManager manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+            Notification.Builder builder = new Notification.Builder(this);
+
+            if(Build.VERSION.SDK_INT>=26) {
+                NotificationChannel channel = new NotificationChannel("id_1", "name_1", NotificationManager.IMPORTANCE_HIGH);
+                manager.createNotificationChannel(channel);
+                builder.setChannelId("id_1");
+            }
+            builder.setContentTitle("new message from topic").setContentText(remoteMessage.getData().get("message")).setSmallIcon(android.R.drawable.star_on);
+
+            manager.notify(1,builder.build());
+
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
 }
